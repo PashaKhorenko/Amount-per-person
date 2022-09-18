0c7399b67f80fd9053f24271a4cc8270c024f7df
Pasha Khorenko <pashakhorenko@gmail.com>
2022-09-18 14:04:47 +0300

    Initial Commit

---
+++ Amount per person/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Amount per person
+//
+//  Created by Паша Хоренко on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
